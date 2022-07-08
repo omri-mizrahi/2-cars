97fbde0c6d98c82c528d863084859d6c9378e39b
omri-mizrahi <omriom5@gmail.com>
2022-07-08 18:05:44 +0300

    finally fixed low fps in mobile build with a simple line of code

--- 2 cars/Assets/Scripts/StartMenu.cs
+++ 2 cars/Assets/Scripts/StartMenu.cs
@@ -6,6 +6,10 @@ public class StartMenu : MonoBehaviour
     #region Variables
     #endregion
 
+    void Awake() {
+        Application.targetFrameRate = 60;
+    }
+
     public void Classic() {
         GameMode.CurrentGameMode = Consts.GameModes.TwoCars;
         SceneManager.LoadScene(Consts.Scenes.TwoCars);        
